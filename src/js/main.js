eb1893650b5a6c26046111e685921c620a84d369
Malin Antonsson <Malin.Antonsson@rga.com>
2016-08-17 21:29:34 +0100

    add moments js cdn

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,13 @@ var site = {
     $('#video').YTPlayer({
       fitToBackground: true,
       videoId: 'AHDATlqQcZQ'
-  });
+    });
+
+    var currentDateTime = new moment();
+    /*window.moment().format();*/
+    var jun = moment("2014-06-01T12:00:00Z");
+    console.log(jun);
+    this.timeZones();
 
     this.ui.navToggle.addEventListener('click', function(e) {
       self.handleNav(navOpen);
@@ -21,6 +27,17 @@ var site = {
   },
   handleNav: function(open) {  
     $(this.ui.body).toggleClass(open);
+  },
+  timeZones: function() {
+    //console.log(window);
+    var now = moment().toObject();; // current timestamp, using your machine's time zone
+    //var nowLA = now.tz('America/Los_Angeles'); // converted to LA time zone
+    //var nowNY = now.tz('America/New_York'); // converted to NY time zone
+    //var jun = window.moment("2014-06-01T12:00:00Z");
+    //console.log(jun);
+    console.log(now);
+    console.log(nowLA);
+    console.log(nowNy);
   }
 
 
