6de313d6b5222a8850b6171a61760be531d345fd
Malin Antonsson <Malin.Antonsson@rga.com>
2016-08-19 19:41:18 +0100

    add moment timezones

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -9,11 +9,12 @@ var site = {
       videoId: 'AHDATlqQcZQ'
     });
 
-    var currentDateTime = new moment();
-    /*window.moment().format();*/
-    var jun = moment("2014-06-01T12:00:00Z");
-    console.log(jun);
+    this.initTimezones();
+
     this.timeZones();
+    setInterval(function() {
+      self.timeZones();
+    }, 500);
 
     this.ui.navToggle.addEventListener('click', function(e) {
       self.handleNav(navOpen);
@@ -23,25 +24,44 @@ var site = {
   ui: {
     navToggle: document.getElementById('toggle-nav'),
     body: document.getElementsByTagName('body')[0],
-    nav: document.getElementById('main-navigation')
+    uk: document.getElementById('time-uk'),
+    paris: document.getElementById('time-paris'),
+    la: document.getElementById('time-los-angeles'),
+    ny: document.getElementById('time-nyc')
   },
   handleNav: function(open) {  
     $(this.ui.body).toggleClass(open);
   },
+  initTimezones: function() {
+    moment.tz.add([
+        'America/Los_Angeles|PST PDT|80 70|0101|1Lzm0 1zb0 Op0',
+        'America/New_York|EST EDT|50 40|0101|1Lz50 1zb0 Op0',
+        'Europe/Paris|CET CEST|-10 -20|01010101010101010101010|1BWp0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00|11e6',
+        "Europe/London|GMT BST|0 -10|01010101010101010101010|1BWp0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00|10e6"
+      ]);
+  },
+
   timeZones: function() {
-    //console.log(window);
-    var now = moment().toObject();; // current timestamp, using your machine's time zone
-    //var nowLA = now.tz('America/Los_Angeles'); // converted to LA time zone
-    //var nowNY = now.tz('America/New_York'); // converted to NY time zone
-    //var jun = window.moment("2014-06-01T12:00:00Z");
-    //console.log(jun);
-    console.log(now);
-    console.log(nowLA);
-    console.log(nowNy);
+    
+    var currentTime = moment().format();
+
+    var ny = moment.tz(currentTime, "America/New_York");
+    var nyTime = ny.format('HH:mm'); 
+
+    var la = moment.tz(currentTime, "America/Los_Angeles");
+    var laTime = la.format('HH:mm'); 
+
+    var paris = moment.tz(currentTime, "Europe/Paris");
+    var parisTime = paris.format('HH:mm'); 
+
+    var london = moment.tz(currentTime, "Europe/London");
+    var londonTime = london.format('HH:mm'); 
+
+    this.ui.uk.innerHTML = londonTime;
+    this.ui.paris.innerHTML = parisTime;
+    this.ui.la.innerHTML = laTime;
+    this.ui.ny.innerHTML = nyTime;
   }
-
-
-
 
 };
 
